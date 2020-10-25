045af327c63a01733366576dfa707209a1822658
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-25 17:00:35 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -7,20 +7,20 @@ class TextAnalyzer
   end
   
   def count_of_word
-    words = text.split(" ")
+    words = @text.split(" ")
     words.count
   end 
   
   def count_of_vowels
-    text.scan(/[aeoui]/).count
+    @text.scan(/[aeoui]/).count
   end 
   
   def count_of_consonants
-    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+    @text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
   
   def most_used_letter
-    s1 = text.gsub(/[^a-z]/, '')
+    s1 = @text.gsub(/[^a-z]/, '')
     arr = s1.split('')
     arr1 = arr.uniq
     arr2 = {}
